5c6273861a7a6514da709f3f4eaf5a07122c203a
swati432936 <swatiboli1212@gmail.com>
2025-03-17 15:28:19 +0530

    done

---
+++ model/Product.js
@@ -0,0 +1,33 @@
+const mongoose=require("mongoose");
+const productSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    price:{
+        type:String,
+        required:true
+    },
+    brand:{
+        type:String,
+        required:true
+    },
+    stock:{
+        type:Number,
+
+    },
+    image:{
+        type:String
+    },
+    description:{
+        type:String,
+
+    },
+    user:{
+        type:mongoose.Schema.ObjectId,
+        ref:'User'
+    },
+})
+
+const Product=mongoose.model('Product',productSchema);
+module.exports={Product};
